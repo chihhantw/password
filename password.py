c778927f3886ffa77c773e9becf4cad2acb7cd35
Clare Lee <chihhantw@gmail.com>
2020-08-08 14:05:28 +0800

    advance done

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 #密碼重試程式
 
 #password = 'a123456'
-
-x = 3
-while x >= 1:
-	password = input('請輸入密碼:')
-	if password == 'a123456':
+password = 'a123456' #存一次
+x = 3 #剩餘的機會
+while x > 0:
+	x = x - 1 #每輸入一次就減少一次機會
+	pwd = input('請輸入密碼:')
+	if pwd == password:
 		print('登入成功!!!')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤!還有', x ,'次機會。' )
+		print('密碼錯誤') #用逗號家字串與數字連結
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+			print('已錯誤三次，登入失敗！')
